48e38e9d4a6d23678e7b742a83c26f9d5c5f29c7
diegocardosodev <diegocardososacramento@gmail.com>
2022-04-19 21:07:46 -0300

    open bank init

---
+++ PowerClasses/openbank/src/test/java/br/com/diego/openbank/OpenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.openbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
