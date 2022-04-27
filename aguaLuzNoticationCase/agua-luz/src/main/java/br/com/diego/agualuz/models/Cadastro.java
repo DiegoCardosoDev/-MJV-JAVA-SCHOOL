421a80903de52d57f7a14d323836ed234b39921e
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-04-27 20:19:02 -0300

    models cadastro

---
+++ aguaLuzNoticationCase/agua-luz/src/main/java/br/com/diego/agualuz/models/Cadastro.java
@@ -0,0 +1,18 @@
+package br.com.diego.agualuz.models;
+
+public class Cadastro {
+
+    Cliente cliente;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+
+
+
+}
